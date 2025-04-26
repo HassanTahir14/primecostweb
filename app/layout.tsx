1a70dd6df269acdee0e018e506a47d50836464a5
HassanTahir14 <hassan.tahir@exirodigital.com>
2025-04-26 21:27:22 +0500

    added favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+      </head>
       <body className={poppins.className}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
